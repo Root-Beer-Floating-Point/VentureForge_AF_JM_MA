c7c03e5b3f03633028aa90d8f51ef7bcd060b5b2
maiza989 <46492293+maiza989@users.noreply.github.com>
2021-11-27 15:19:16 -0500

    27/11/21 | Maitham | Worked on the basic layout for create a new module and added navigation | 1 hour and 45 minutes

---
+++ VentureForge/CreateModule.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VentureForge
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class CreateModule : Window
+    {
+        public CreateModule()
+        {
+            InitializeComponent();
+        }
+        private void UploadAssests_Click(object sender, RoutedEventArgs e)
+        {
+            new UploadAssests().Show();
+        }
+
+        private void ExamineAssets_Click(object sender, RoutedEventArgs e)
+        {
+            new ExamineExistingAssests().Show();
+        }
+
+        private void CreateSheets_Click(object sender, RoutedEventArgs e)
+        {
+            new CreateSheets().Show();
+        }
+
+        private void ExamineSheets_Click(object sender, RoutedEventArgs e)
+        {
+            new ExamineExistingSheets().Show();
+        }
+
+        private void UploadModule_Click(object sender, RoutedEventArgs e)
+        {
+            new UploadeModule().Show();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+        }
+
+        private void SginIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
