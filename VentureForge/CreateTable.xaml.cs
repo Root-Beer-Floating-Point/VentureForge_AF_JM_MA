4379f71533a64bd15ebe214d2e1d6afb8e58f1b8
JKBizcuits <jmorris7@bellarmine.edu>
2021-11-28 22:45:30 -0500

    11/28/21 | Jacob | Added basic format for remaining pages and corrected git issues | 5 hours

---
+++ VentureForge/CreateTable.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VentureForge
+{
+    /// <summary>
+    /// Interaction logic for CreateTable.xaml
+    /// </summary>
+    public partial class CreateTable : Window
+    {
+        public CreateTable()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e) //Return to Home Page
+        {
+
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e) //Return to whatever the previous page is
+        {
+
+        }
+
+        private void CreateAsset_Click(object sender, RoutedEventArgs e) //Create an asset to be added to the table
+        {
+
+        }
+
+        private void AddAsset_Click(object sender, RoutedEventArgs e) //add an asset? Should be reviewed again to see what purpose it fills
+        {
+
+        }
+    }
+}
