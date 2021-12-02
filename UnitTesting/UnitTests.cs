394564aa3c10694ce94b2bb7d8b78d8ef9b0aab2
Adam Fischer <adfisch712@outlook.com>
2021-12-01 20:02:18 -0500

    12/1/21 -- Got NUnit tests done, will need to copy this over for the stuff that will be dockerized -- 3 hours

--- UnitTesting/UnitTests.cs
+++ UnitTesting/UnitTests.cs
@@ -1,18 +1,32 @@
 using NUnit.Framework;
 
+
+
 using System.Collections.Generic;
 
+
+
 using System.Windows;
+
+
 
 using VentureForge;
 
 
 
+
+
 namespace UnitTesting
+
+
 
 {
 
+
+
     public class UnitTests
+
+
 
     {
 
@@ -20,11 +34,19 @@ namespace UnitTesting
 
 
 
+
+
         [SetUp]
+
+
 
         public void Setup()
 
+
+
         {
+
+
 
 
 
@@ -32,22 +54,36 @@ namespace UnitTesting
 
 
 
+
+
         [Test]
+
+
 
         public void Test1()
 
+
+
         {
 
+
+
             string modtest = "";
-            
+
             Sheet sh1 = new(); sh1.name = "sheet1";
             Sheet sh2 = new(); sh2.name = "sheet2";
+
+
 
             Module mod = new Module("mod1");
 
 
 
+
+
             mod.AddSheet("sheet1", sh1); mod.AddSheet("sheet2", sh2);
+
+
 
             string test = "sheet1sheet2";
 
@@ -55,60 +91,135 @@ namespace UnitTesting
 
 
 
+
+
             Assert.IsTrue(mod.name == "mod1");
+
+
 
             Assert.AreNotSame(test, modtest);
 
 
 
+
+
             foreach (KeyValuePair<string, Sheet> ele1 in mod.SheetList)
+
+
 
             {
 
+
+
                 modtest += ele1.Key;
+
+
 
             }
 
+
+
             Assert.AreEqual(test, modtest);
 
+
+
         }// end test1
+
+
 
         [Test]
         public void Test2()
         {
 
-            Sheet sh = new(); 
+
+
+            Sheet sh = new();
             sh.addContainer("con1", true, false); sh.addContainer("con2", true, true);
-            Container con1 = sh.containerList[0]; Container con2 = sh.containerList[1];
-            
+            Container con1 = sh.containerList["con1"]; Container con2 = sh.containerList["con2"];
+
             Assert.IsFalse(con1.integer); Assert.IsTrue(con2.integer);
             Assert.AreNotEqual(con1.name, con2.name);
             Assert.AreEqual(con1.name, "con1"); Assert.AreEqual(con2.name, "con2");
 
+
+
         }// end test2
+
+
 
         [Test]
         public void Test3()
         {
 
+
+
             Sheet sh1 = new();
             Container con1 = new Container("con1"); Container con2 = new Container("con2"); Container con3 = new Container("con3");
             sh1.addContainer(con1.name, false, false); sh1.addContainer(con2.name, false, false); sh1.addContainer(con3.name, false, false);
 
+
+
             string test = "con1con2con3"; string modtest = "";
 
-            foreach (Container ele1 in sh1.containerList)
+
+
+            foreach (KeyValuePair<string, Container> ele1 in sh1.containerList)
+
+
 
             {
 
-                modtest += ele1.name;
+
+
+                modtest += ele1.Key;
+
+
 
             }
 
+
+
             Assert.AreEqual(test, modtest);
+
+
 
         }// end test 3
 
+
+
+        [Test]
+        public void Test4()
+        {
+            Container con1 = new Container();
+            Assert.IsTrue(con1.name == "test");
+
+
+
+            Container con2 = new Container("con2", true);
+            Assert.IsTrue(con2.integer);
+
+
+
+            Sheet sh1 = new();
+            sh1.containerList.Add(con1.name, con1); sh1.containerList.Add(con2.name, con2);
+
+
+
+            Assert.AreNotSame(sh1.getContainer("test"), sh1.getContainer("con2"));
+
+
+
+            Assert.AreSame(sh1.getContainer("test"), con1);
+            Assert.AreSame(sh1.getContainer("con2"), con2);
+
+
+
+        }
+
+
+
     }
+
+
 
 }
